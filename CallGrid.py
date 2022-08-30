1ea05f748962aec8aa5a138cee863949f32fee60
jim fisher <jedijf@myfisher.org>
2022-08-29 23:02:15 -0400

    Add files via upload

---
+++ CallGrid.py
@@ -0,0 +1,27 @@
+#CAll grid grabber
+
+#!/usr/bin/python3
+
+
+import requests
+import json
+
+callsign = ""
+
+while 1==1:
+
+    callsign = input("Enter Callsign: <q> to QUIT  >> ")
+    
+    if callsign == "q" :
+        break
+
+    else:
+        try:
+            api_url = f"https://callook.info/{callsign}/json"
+            response = requests.get(api_url)
+            location = response.json()["location"]
+            grid = location["gridsquare"]
+            print (f"{callsign} GRID is {grid}")
+            
+        except:
+            print (f"CALLSIGN {callsign} INVALID  <q> to QUIT")
